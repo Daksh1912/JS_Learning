6b3f5c978ab53e8464801819e661ef962c7057e7
Daksh1912 <138623381+Daksh1912@users.noreply.github.com>
2024-12-20 16:55:13 +0000

    memory types

--- 01_Basics/dataTypes-Summary.js
+++ 01_Basics/dataTypes-Summary.js
@@ -24,3 +24,35 @@ const myFunction = function() {
 console.log(typeof myFunction);
 console.log(typeof Id);
 
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive) and Heap Memory(Non Primitive)  - We get reference for heap 
+// while a copy of that element in stack memory
+
+// Stack memory are used in Primitive Data Type 
+// while non primitive data types are used in Heap memory 
+
+// example for stack memory
+let emailId = "daksh@gmail.com"
+let anotherEmailId = emailId
+anotherEmailId = "sharma@yahoo.com"
+
+console.log(emailId);
+console.log(anotherEmailId);
+
+
+// example for heap memory 
+let userOne = {
+    email: "madhav@gmail.com",
+    upi: "12312@ybl",
+}
+let userTwo = userOne
+userTwo.email = "cricket@play.com"  
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
