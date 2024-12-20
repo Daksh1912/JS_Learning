245e257714836710eec48d8ac11328fe19136640
Daksh1912 <138623381+Daksh1912@users.noreply.github.com>
2024-12-20 15:54:42 +0000

    learned operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -23,3 +23,18 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// **************************************  Operations **********************************
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+let num1, num2 , num3
+num1 = num2 = num3 = 2 + 2
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
