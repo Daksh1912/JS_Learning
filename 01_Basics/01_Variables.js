38f50b68fd2c78c55d15caaccc7b059b996b2632
Daksh1912 <138623381+Daksh1912@users.noreply.github.com>
2024-12-20 15:05:06 +0000

    added comments

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -3,4 +3,8 @@ let accountEmail  = "dakhs@gmail.com"
 var accountPassword = "12345"
 accountCity = "Ghaziabad"
 let accountState;
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
